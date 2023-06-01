022e51fd80eb0d4b025b99efb79bb8b0518f9f67
DARONGREN <wingmurasaki@gmail.com>
2023-06-01 17:34:20 -0400

    remove secret

--- netapi/server.js
+++ netapi/server.js
@@ -2,23 +2,27 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/UserRoutes')
+const dotenv = require('dotenv')
 
 const app = express()
-
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-mongoose
-  .connect(
-    'mongodb+srv://dren9:1340abc@cluster0.srbt38q.mongodb.net/?retryWrites=true&w=majority'
-  )
-  .then(() => {
-    console.log('DB Connetion Successfull')
-  })
-  .catch((err) => {
-    console.log(err.message)
-  })
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log('connect to DB')
+    })
+    .catch((err) => {
+      throw err
+    })
+}
 
 app.use('/api/user', userRoutes)
 
-app.listen(5000, console.log('server start'))
+app.listen(5000, () => {
+  connect()
+  console.log('connected')
+})
